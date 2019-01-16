e8c0dcc69f6a77982296bccae001bbd572dbe43e
Kajetan Puchalski <cajetan.puchalski@gmail.com>
2019-01-16 12:45:55 +0100

    [Position Monitor] Fetching ticker prices

--- position-monitor.py
+++ position-monitor.py
@@ -7,9 +7,24 @@
 #
 #
 #
-#
-#
-#
-#
 
-print("Position monitor")
+
+import json
+import requests
+
+print("Position monitor\n")
+
+def get_ticker_info(ticker_data):
+    return "{} {}".format(ticker_data['symbol'], ticker_data['regularMarketPrice'])
+
+url = "https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=NUGT,HSBA.L,LHA.DE,UG.PA,FORTUM.HE&fields=regularMarketPrice"
+
+response = requests.request("GET", url)
+ticker_quotes = [t for t in json.loads(response.text)['quoteResponse']['result']]
+
+for t in ticker_quotes:
+    print(get_ticker_info(t))
+
+
+
+
