647c3836fcc06de439c7cdc8dd26b813fd96c7f4
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-11-13 12:23:00 +0000

    [Forex] Split sessions into before and after closing

--- market-helper.py
+++ market-helper.py
@@ -2,12 +2,17 @@ from termcolor import colored
 import time
 from datetime import datetime, timedelta
 
-def today_london_session(date):
+def tokyo_session(date):
+    open = date.replace(hour=00, minute=0, second=0, microsecond=0)
+    close = date.replace(hour=9, minute=0, second=0, microsecond=0)
+    return (open, close)
+
+def london_session(date):
     open = date.replace(hour=8, minute=0, second=0, microsecond=0)
     close = date.replace(hour=16, minute=0, second=0, microsecond=0)
     return (open, close)
 
-def today_new_york_session(date):
+def new_york_session(date):
     open = date.replace(hour=13, minute=0, second=0, microsecond=0)
     close = date.replace(hour=22, minute=0, second=0, microsecond=0)
     return (open, close)
@@ -16,31 +21,54 @@ def clean_timedelta(delta):
     return str(delta).split(".")[0]
 
 now = datetime.now()
-london = today_london_session(now)
-new_york = today_new_york_session(now)
+london = london_session(now)
+new_york = new_york_session(now)
+tokyo = tokyo_session(now)
 
 # hour_ago = now - timedelta(hours=1)
 # yesterday = now - timedelta(days=1)
 
 # Time changer for testing
-now = now.replace(hour=23, minute=0, second=0, microsecond=0)
+# now = now.replace(hour=23, minute=0, second=0, microsecond=0)
 
-print("The time is:", now.strftime("%H:%M:%S %p"), "\n")
+print("The time is:", colored(now.strftime("%H:%M:%S %p"), 'yellow'), "\n")
 
-if now >= london[0] and now <= london[1]:
-    time_to_close = london[1] - now
-    print("London open, closes in {}".format(clean_timedelta(time_to_close)))
-else:
-    # TODO: differentiate before close and after close
+print(colored('Forex', 'blue'), "\n")
+
+## Tokyo
+
+if now >= tokyo[0] and now <= tokyo[1]: # Opened
+    time_to_close = tokyo[1] - now
+    print("Tokyo open, closes in {}".format(clean_timedelta(time_to_close)))
+else: # After open
+    next_tokyo = tokyo_session(now + timedelta(days=1))
+    time_to_open = next_tokyo[0] - now
+    print("Tokyo closed, opens in {}".format(clean_timedelta(time_to_open)))
+
+## London
+
+if now < london[0]: # Before open
     time_to_open = london[0] - now
     print("London closed, opens in {}".format(clean_timedelta(time_to_open)))
+elif now >= london[0] and now <= london[1]: # Open
+    time_to_close = london[1] - now
+    print("London open, closes in {}".format(clean_timedelta(time_to_close)))
+else: # After open
+    next_london = london_session(now + timedelta(days=1))
+    time_to_open = next_london[0] - now
+    print("London closed, opens in {}".format(clean_timedelta(time_to_open)))
 
-if now >= new_york[0] and now <= new_york[1]:
+## New York
+
+if now < new_york[0]:
+    time_to_open = new_york[0] - now
+    print("New York closed, opens in {}".format(clean_timedelta(time_to_open)))
+elif now >= new_york[0] and now <= new_york[1]:
     time_to_close = new_york[1] - now
     print("New York open, closes in {}".format(clean_timedelta(time_to_close)))
 else:
-    # TODO: differentiate before close and after close
-    time_to_open = new_york[0] - now
+    next_new_york = new_york_session(now + timedelta(days=1))
+    time_to_open = next_new_york[0] - now
     print("New York closed, opens in {}".format(clean_timedelta(time_to_open)))
 
 
