0d8f51a81dcb91ca70340d440fd9dee28c535258
Kajetan Puchalski <cajetan.puchalski@gmail.com>
2020-01-16 13:50:48 +0000

    [Position Monitor] General ideas

---
+++ position-monitor.py
@@ -0,0 +1,15 @@
+
+# Some data persistence for storing tickers to watch
+# Processing yahoo finance ticker data to fetch P&L
+# Live refresh (?)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+print("Position monitor")
